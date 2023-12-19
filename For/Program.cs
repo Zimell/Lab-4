a294e40a8ae4546c49ad3cb609eb60dd86cbf4d6
Makarov_Slava <zim_zimovich00@mail.ru>
2023-12-19 08:28:50 +0300

    Добавьте файлы проекта.

---
+++ For/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите X = ");
+            int n = int.Parse(Console.ReadLine());
+            int z = 3;
+            int x = 4;
+            int answer = 0;
+            for (int k = 1; k <= n; k++)
+            {
+                answer += z * x;
+                z = z + 3;
+                x = x + 2;
+            }
+            Console.WriteLine($"Ответ сумма ряда = {answer}");
+        }
+    }
+}
